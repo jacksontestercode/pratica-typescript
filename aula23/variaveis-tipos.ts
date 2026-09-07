ced3d2a5a4e778e35e28476071db0399b456eeaa
Jackson Mendes <jacksonfla6@gmail.com>
2026-09-07 18:16:57 -0300

    feat: (aula23) sobre variaveis tipos e nomenclaturas da pasta e arquivos

---
+++ aula23/variaveis-tipos.ts
@@ -0,0 +1,13 @@
+let statusPedido = 'Pendente'
+let primeiroNome: string = 'Maria'
+let ativo = false
+let ativado: boolean = true
+let precoUnitario = 234.4
+let precoTotal: number = 300
+
+
+const BASE_URL = 'www.google.com.br'
+
+console.log(ativo)
+console.log('O nome do usuário é: ', primeiroNome)
+console.log('O Status do pedido é: ', statusPedido)
