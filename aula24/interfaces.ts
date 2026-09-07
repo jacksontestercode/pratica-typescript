b050557c83203a88cb82e7d4b4f8f01997ec359d
Jackson Mendes <jacksonfla6@gmail.com>
2026-09-07 19:20:36 -0300

    fest: usando ia

--- aula24/interfaces.ts
+++ aula24/interfaces.ts
@@ -1,38 +1,27 @@
-interface Produtos {
-
-    id: number,
-    name: string,
-    slug: string,
-    price: number,
-    stock_quantity: number,
-    sku: string,
-    category_id: number,
-    supplier_id: number,
-    is_active: boolean,
-
+interface Product {
+  id: number;
+  name: string;
+  slug: string;
+  price: number;
+  stock_quantity: number;
+  sku: string;
+  category_id: number;
+  supplier_id: number;
+  is_active: boolean;
+  image_url: string | null;
+  pdf_url: string | null;
+  sales_count: number;
+  reviews_count: number;
+  rating: number;
+  created_at: string;
+  categories: Category;
+  suppliers: Supplier;
 }
 
-
-const meuProduto: Produtos = {
-    id: 1,
-    name: 'Jackson',
-    slug: 'Teste',
-    price: 12.3,
-    stock_quantity: 100,
-    sku: 'Teste',
-    category_id: 1,
-    supplier_id: 1,
-    is_active: true
+interface Category {
+  name: string;
 }
 
-console.log('---Validando Produtos---')
-console.log('ID:', meuProduto.id);
-console.log('Nome:', meuProduto.name);
-console.log('Slug:', meuProduto.slug);
-console.log('Preço:', meuProduto.price);
-console.log('Estoque:', meuProduto.stock_quantity);
-console.log('SKU:', meuProduto.sku);
-console.log('Categoria:', meuProduto.category_id);
-console.log('Fornecedor:', meuProduto.supplier_id);
-console.log('Ativo:', meuProduto.is_active);
-
+interface Supplier {
+  company_name: string;
+}
